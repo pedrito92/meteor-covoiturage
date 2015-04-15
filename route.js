959e58ca5b2738968934868c0416b9610c3bd8a2
Pierre <pierre.fayol@cifacom.com>
2015-04-15 13:40:45 +0200

    Add other routes

--- route.js
+++ route.js
@@ -2,7 +2,33 @@ Router.route('/', function () {
   this.render('home');
 });
 
+Router.route('/create', function () {
+  this.render('create');
+});
+
 Router.route('/covoiturage/:_id', function () {
+  this.render('covoiturage', {
+    data: function () {
+      // set the reactive state variable "postId" with a value
+      // of the id from our url
+      this.state.set('_id', this.params._id);
+      // this.render();
+    }
+  });
+});
+
+Router.route('/profil/:_id/annonces', function () {
+  this.render('covoiturage', {
+    data: function () {
+      // set the reactive state variable "postId" with a value
+      // of the id from our url
+      this.state.set('_id', this.params._id);
+      // this.render();
+    }
+  });
+});
+
+Router.route('/profil/:_id/resa', function () {
   this.render('covoiturage', {
     data: function () {
       // set the reactive state variable "postId" with a value

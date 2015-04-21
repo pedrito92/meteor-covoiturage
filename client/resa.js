e2fcacb108e1ae2e8ac14e1dd093be79bee0b1fd
pedrito92 <pierre.fayol@cifacom.com>
2015-04-21 16:52:22 +0200

    Add list of user's resevation

--- client/resa.js
+++ client/resa.js
@@ -2,5 +2,18 @@
  * Created by Pierre on 18/04/15.
  */
 Template.resa.helpers({
+    trajets : function () {
+        return trajets.find({inscrit:{ $elemMatch: { user_id:  Meteor.userId() }}});
+    },
+    getUser: function (userId) {
+        if (userId) {
+            var user = Meteor.users.find({_id : userId}),
+                username = '';
+            user.forEach(function (u) {
+                username = u.username;
+            });
 
+            return username;
+        }
+    }
 });
